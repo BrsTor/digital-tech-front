cfcc7c911a9e8046bc07c98fb88e28666c101872
brs_tor <boristor22@gmail.com>
2024-04-03 17:16:25 -0400

    Added form group for sign up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
+import { SignUpComponent } from "./sign-up/sign-up.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [RouterOutlet, HeaderComponent]
+    imports: [RouterOutlet, HeaderComponent, SignUpComponent, ]
 })
 export class AppComponent {
   title = 'digital-tech-front';
