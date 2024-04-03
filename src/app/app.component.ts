56c340a8ccc561b1df2c8540a70a744cffadced4
brs_tor <boristor22@gmail.com>
2024-04-03 19:47:38 -0400

    reverting commits to clean the master branch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./header/header.component";
-import { SignUpComponent } from "./sign-up/sign-up.component";
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss',
-    imports: [RouterOutlet, HeaderComponent, SignUpComponent, ]
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'digital-tech-front';
