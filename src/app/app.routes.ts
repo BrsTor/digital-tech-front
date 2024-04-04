fcb5a42966e80fc44a70ad2d180d2ee3c446b7cc
brs_tor <boristor22@gmail.com>
2024-04-03 22:26:29 -0400

    Added router services and redirect to homepage after signup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: SignUpComponent },
+  { path: 'homepage', component: HomepageComponent },
+];
--- src/app/services/sign-up/sign-up.service.ts
+++ src/app/services/sign-up/sign-up.service.ts
@@ -1,18 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SignUpService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   createUser(body: any) {
     this.http.post(environment.baseApiUrl+'auth/signup', body).subscribe(resp => {
-      console.log(resp)
-      /* TODO - Redirect to homepage */
+      this.router.navigate(['/homepage'])
     }, err => {
       alert('Error Code: '+err.status+'. '+err.statusText +'\n'+err.error.message)
     })
