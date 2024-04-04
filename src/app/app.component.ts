401f1eb25f4edd34e50e06248903d992654b96a6
brs_tor <boristor22@gmail.com>
2024-04-03 21:42:30 -0400

    Refactor of folders and signup complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./header/header.component";
-import { SignUpComponent } from "./sign-up/sign-up.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { SignUpComponent } from "./components/sign-up/sign-up.component";
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [RouterOutlet, HeaderComponent, SignUpComponent, ]
+    imports: [RouterOutlet, HeaderComponent, SignUpComponent, HttpClientModule ]
 })
 export class AppComponent {
   title = 'digital-tech-front';
--- src/app/components/sign-up/sign-up.component.ts
+++ src/app/components/sign-up/sign-up.component.ts
@@ -1,24 +1,30 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { FormControl, FormGroup, NgForm, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { SignUpService } from '../../services/sign-up/sign-up.service';
 
 @Component({
   selector: 'sign-up',
   standalone: true,
   imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './sign-up.component.html',
-  styleUrl: './sign-up.component.scss'
+  styleUrl: './sign-up.component.scss',
+  providers: [SignUpService]
 })
 
 export class SignUpComponent implements OnInit {
 
   signUpForm!: FormGroup;
+  userRoles = ['admin', 'user'];
+
+  constructor(private sigUpService: SignUpService) {}
 
   ngOnInit(): void {
     this.signUpForm = new FormGroup({
       'name': new FormControl(null, [Validators.required, Validators.minLength(2), Validators.maxLength(25)]),
       'surname': new FormControl(null, [Validators.required, Validators.minLength(2), Validators.maxLength(25)]),
       'username': new FormControl(null, [Validators.required, Validators.minLength(5), Validators.maxLength(25)]),
+      'role': new FormControl('user'),
       'avatar': new FormControl(null)
     })
   }
@@ -26,7 +32,7 @@ export class SignUpComponent implements OnInit {
 
 
   onSubmit() {
-    console.log(this.signUpForm.value);
+    this.sigUpService.createUser(this.signUpForm.value)
   }
 
 
